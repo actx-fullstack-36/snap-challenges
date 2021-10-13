ecd3faae9985becc96a2fba84da0273f6111150e
George Kephart <gkephart@cnm.edu>
2021-10-13 08:01:19 -0600

    added the first answer for the functions prompt

---
+++ functions/functions.js
@@ -0,0 +1,18 @@
+//Function #1
+// Take two numbers as arguments and return a new value after a mathematical operation (addition, subtraction, multiplication, etc.) Which operation is your choice.
+
+
+/**
+ * A simple function that multiplies two values
+ *
+ * @param {number} number1 first number to be multiplied
+ * @param {number} number2 second number to be multiplied
+ * @return {number} the result from multiplying number1 and number2
+ **/
+function multiplyTwoNumbers(number1, number2) {
+    let result = number1 * number2
+    return result
+}
+
+const result = multiplyTwoNumbers(3,4)
+console.log(result)
