e5fbf3fa178f889b3045b4cefb3613c41d77fc62
George Kephart <gkephart@cnm.edu>
2021-10-20 08:12:42 -0600

    added answer to today syntax code along

---
+++ js-objects/js-objects.js
@@ -0,0 +1,31 @@
+const fruits = ["apples", "oranges", "bananas"]
+fruits.push("strawberries")
+fruits.sort()
+
+
+const person = {
+    name: "Jim",
+    age: 35,
+    isAdult: true,
+    hairColor: "blonde",
+    run: function (){return console.log("running")}
+}
+
+person.run()
+
+1 //numbers anything that have mathematical operations preformed on it
+"thththth" // normal values
+true // true or flase values
+function doSomething() {
+    return "I did something"
+}
+
+const iDidSomething = doSomething
+[0,1,2,3,4,5,6]
+undefined
+const object = {one: "two", fortyTwo: true}
+
+
+const complexObject = {person: person, fruits: fruits}
+
+const persons = [person]
